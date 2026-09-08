b8c94452ca32dfecc1101dc9654d8a8ad91cdd02
Miguel Hormigo <m99hormigo@gmail.com>
2026-09-08 10:08:12 +0200

    Add: valid-sudoku - submission-9

---
+++ Data Structures & Algorithms/valid-sudoku/submission-9.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        columns = [0] * 9
+
+        for r in range(9):
+            row = 0
+            if r % 3 == 0:
+                squares = [0] * 3
+
+            for c in range(9):
+
+                n = board[r][c]
+                if n != '.':
+                    d = 1 << int(n)
+                    if d & row or d & columns[c] or d & squares[c // 3]:
+                        return False
+                    
+                    row |= d
+                    columns[c] |= d
+                    squares[c // 3] |= d
+        
+        return True
